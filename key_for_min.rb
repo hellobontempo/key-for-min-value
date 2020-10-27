a724318410ad9a34c0f7fc769dfc1b13bde53818
claire <clairebontempo@gmail.com>
2020-10-26 17:26:14 -0700

    commit tree 62a44aeb4b1b61127f53bf630df6d175644cdee6

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,23 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
+    if hash.empty?
+        return nil
+    end
 
+    lowest_number = nil
+    putmeout = ""
+    hash.each do |name, age|
+        if lowest_number == nil  
+            lowest_number = age 
+                putmeout = name
+        elsif age < lowest_number
+            lowest_number = age
+            putmeout = name
+        end
+    
+    end
+    return putmeout
+    #hash.key(lowest_number)
 end
